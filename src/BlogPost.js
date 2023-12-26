a51e93ff3981f8cf04565bd88c8d125df908bf8c
Dhruv Savla <sdhruv0405@gmail.com>
2023-12-26 13:51:36 -0800

    dropdown for category

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Editor } from 'primereact/editor';
 import { useState } from 'react';
 import { Button } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function BlogPost() {
   const [value, setValue] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
+  const [category, setCategory] = useState('');
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -33,6 +41,28 @@ function BlogPost() {
         <h3 style={{marginRight: "10px"}}>Enter the title for your Blog :</h3>
         <input type="text" style={{ width: "20%", height: "20px" }} />
       </div>
+
+      <div className='category' style={{ marginLeft: '100px', marginTop: "20px", border: "1px solid #dsdcdc", display: "flex" }}>
+      <h3 style={{marginRight: "10px"}}>Choose a category : </h3>
+      <FormControl sx={{ m: 1, minWidth: 150}}>
+        <InputLabel id="demo-simple-select-autowidth-label">Category</InputLabel>
+        <Select style={{ }}
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={category}
+          onChange={handleChange}
+          autoWidth
+          label="Category"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Twenty</MenuItem>
+          <MenuItem value={21}>Twenty one</MenuItem>
+          <MenuItem value={22}>Twenty one and a half</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
 
       <div className='post' style={{ marginLeft: '100px', marginRight: '100px', marginTop: '30px' }}>
         <Editor style={{ height: "750px" }}
